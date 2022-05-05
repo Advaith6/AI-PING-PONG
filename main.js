6b6d1c3c9d13baf15c59c3691a85ce20c5a71fd4
Advath Coder <72031535+Advaith6@users.noreply.github.com>
2022-05-05 12:53:28 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+var paddle2 = 10, paddle1 = 10;
+
+var paddle1X = 10, paddle1Height = 110;
+var paddle2Y = 685, paddle2Height = 70;
+
+var score1 = 0, score2 = 0;
+var paddle1Y;
+
+var playerscore = 0;
+
+var pcscore = 0;
+
+var ball = {
+    x: 350 / 2,
+    y: 480 / 2,
+    r: 20,
+    dx: 3,
+    dv: 3
+}
+
 rightWristY = 0;
 rightWristX = 0;
 scoreRightWrist = 0;
@@ -5,8 +25,8 @@ scoreRightWrist = 0;
 game_status = "";
 
 function preload() {
-    ball_touch_paddel = loadSound("ball_touch_paddel.wav")
-    missed = loadSound("missed.wav")
+    ball_touch_paddel = loadSound("ball_touch_paddle.mp3")
+    missed = loadSound("missed.mp3")
 }
 
 function setup() {
@@ -55,6 +75,45 @@ function draw() {
         fill("red");
         stroke("red");
         circle(rightWristX, rightWristY, 30);
+    }
+    if (gmae_status == "start") {
+        document.getElementById("status").innerHTML = "Game Is Loaded";
+        paddleInCanvas();
+
+        fill(250, 0, 0);
+        stroke(0, 0, 250);
+        strokeWeight(0.5);
+        paddle1Y = rightWristY;
+        rect(paddle1X, paddle1Y, paddle1, paddle1Height, 100);
+
+        fill("#FFA500");
+        stroke("#FFA500");
+        var paddle2y = ball.y - paddle2Height / 2;
+        rect(paddle2Y, paddle2y, paddle2, paddle2Height, 100);
+
+        midline();
+
+        drawScore();
+
+        models();
+
+        move();
+    }
+}
+
+function reset() {
+    ball.x = width / 2 + 100;
+    ball.y = height / 2 + 100;
+    ball.dx = 3;
+    ball.dy = 3;
+}
+
+function midline() {
+    for (i = 0; i < 480; i += 10) {
+        var y = 0;
+        fill("white");
+        stroke(0);
+        rect(width / 2, y + i, 10, 480);
     }
 }
 
