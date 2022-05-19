cbbf7ebb1a22e9f631a55d262edb99e044e00fa9
Advath Coder <72031535+Advaith6@users.noreply.github.com>
2022-05-19 12:48:52 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -117,5 +117,52 @@ function midline() {
     }
 }
 
+function drawScore() {
+    textAlign(CENTER);
+    textSize(20);
+    fill("white");
+    stroke(250, 0, 0);
+    text("Player:", 100, 50);
+    text(playerscore, 150, 50);
+    text("Computer:", 500, 50);
+    text(pcscore, 555, 50);
+}
 
-
+function move() {
+    fill(50, 350, 0);
+    stroke(255, 0, 0);
+    strokeWeight(0.5);
+    ellipse(ball.x, ball.y, ball.r, 20);
+    ball.x = ball.x + ball.dx;
+    ball.y = ball.y + ball.dy;
+    if (ball.x + ball.r > width - ball.r / 2) {
+        b.dx = -ball.dx - 0.5
+    }
+    if (ball.x - 2.5 * ball.r / 2 < 0) {
+        if (ball.y >= paddle1Y && ball.y <= paddle1Y + paddle1Height) {
+            ball.dx = -ball.dx + 0.5;
+            ball_touch_paddel.play();
+        }
+        else {
+            pcscore++;
+            missed.play();
+            reset();
+            navigator.vibrate(100);
+        }
+    }
+    if (pcscore == 4) {
+        fill("#FFA500");
+        stroke(0);
+        rect(0, 0, width, height - 1);
+        fill("white");
+        stroke("white");
+        textSize(25);
+        text("Game Over!", width / 2, height / 2);
+        text("Press restart button to play again!", width / 2, height / 2 + 30);
+        noLoop();
+        pcscore = 0;
+    }
+    if (ball.y + ball.r > height || ball.y - ball.r < 0) {
+        ball.dy = -ball.dy;
+    }
+}
